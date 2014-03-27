6006434628241a83591efcd92f9830e841a3c722
seville <seville@yahoo-inc.com>
2014-03-26 22:27:01 -0700

    Initial Commit

---
+++ ASTweet/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  ASTweet
+//
+//  Created by Alexander Seville on 3/26/14.
+//  Copyright (c) 2014 Alexander Seville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ASTweetTests/ASTweetTests.m
@@ -0,0 +1,34 @@
+//
+//  ASTweetTests.m
+//  ASTweetTests
+//
+//  Created by Alexander Seville on 3/26/14.
+//  Copyright (c) 2014 Alexander Seville. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ASTweetTests : XCTestCase
+
+@end
+
+@implementation ASTweetTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
